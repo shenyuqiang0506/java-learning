7a6353c68c4254d98163adde456e67817252977e
shen <2861476552@qq.com>
2023-11-07 11:48:03 +0000

    删除文件 src

--- src/AutoConvert.java
+++
@@ -1,9 +0,0 @@
-public class AutoConvert {
-	public static void main (String[] args) {
-      //演示
-		int num = 'a';//对的
-		double d1 = 80;//对的
-		System.out.println(num);
-		System.out.println(d1);
-	}
-}
--- src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * @Author 申宇强
- * @Date 2023/4/14 18:30
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
