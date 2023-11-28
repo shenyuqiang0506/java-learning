cd0e40cfdf210cdfc5e23c18a6def031c0e745c6
山河不入心 <2861476552@qq.com>
2023-11-28 16:38:40 +0800

    接口练习

--- chapter10/shen/src/com/interface_/InterfaceExercise02.java
+++ chapter10/shen/src/com/interface_/InterfaceExercise02.java
@@ -5,4 +5,25 @@ package com.interface_;
  * @Date 2023/11/8 16:54
  */
 public class InterfaceExercise02 {
+    public static void main(String[] args) {
+        new C().pX();
+    }
+}
+
+interface A1 {
+    int x = 0;//想到 等价 public static final int x = 0;
+}
+
+class B1 {
+    int x = 1;
+}//普通属性
+
+class C extends B1 implements A1 {
+    public void pX() {
+        //System.out.println(x); //错误，原因不明确x
+        //可以明确的指定x
+        //访问接口的 x 就使用 A.x
+        //访问父类的 x 就使用 super.x
+        System.out.println(A1.x + " " + super.x);
+    }
 }
