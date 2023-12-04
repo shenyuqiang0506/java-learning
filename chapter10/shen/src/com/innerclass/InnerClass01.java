40f8fc042e9884f2b9bc44ffb132b5b7b6d84e7b
山河不入心 <2861476552@qq.com>
2023-12-04 11:08:03 +0800

    内部类入门

---
+++ chapter10/shen/src/com/innerclass/InnerClass01.java
@@ -0,0 +1,31 @@
+package com.innerclass;
+
+/**
+ * @Author 申宇强
+ * @Date 2023/11/28 20:10
+ */
+public class InnerClass01 {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Outer {//外部类
+    private int n1 = 100;//属性
+
+    public  Outer(int n1) {
+        this.n1 = n1;
+    }
+
+    public void m1() {//方法
+        System.out.println("m1()");
+    }
+
+    {//代码块
+        System.out.println("代码块...");
+    }
+
+    class Inner {//内部类, 在Outer类的内部
+
+    }
+}
