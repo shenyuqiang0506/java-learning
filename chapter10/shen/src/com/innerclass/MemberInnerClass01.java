7ae092ae07f92c65e0d6fc4ec9aa6bd42dd9315e
山河不入心 <2861476552@qq.com>
2023-12-12 16:25:00 +0800

    成员内部类的使用

---
+++ chapter10/shen/src/com/innerclass/MemberInnerClass01.java
@@ -0,0 +1,56 @@
+package com.innerclass;
+
+/**
+ * &#064;Author  申宇强
+ * &#064;Date  2023/12/12 15:58
+ */
+public class MemberInnerClass01 {
+    public static void main(String[] args) {
+        Outer08 outer08 = new Outer08();
+        outer08.t1();
+        //外部其他类，使用成员内部类的三种方式
+        //老韩解读
+        // 第一种方式
+        // outer08.new Inner08(); 相当于把 new Inner08()当做是outer08成员
+        // 这就是一个语法，不要特别的纠结.
+        Outer08.Inner08 inner08 = outer08.new Inner08();
+        inner08.say();
+        // 第二种方式 在外部类中，编写一个方法，可以返回 Inner08对象
+        Outer08.Inner08 inner08Instance = outer08.getInner08Instance();
+        inner08Instance.say();
+    }
+}
+
+class Outer08 {//外部类
+    private int n1 = 10;
+    public String name = "张三";
+
+    public void hi() {
+        System.out.println("hi()方法");
+    }
+
+    //方法，返回一个Inner08实例
+    public Inner08 getInner08Instance() {
+        return new Inner08();
+    }
+
+    //1.注意: 成员内部类，是定义在外部内的成员位置上
+    //2.可以添加任意访问修饰符(public、protected 、默认、private),因为它的地位就是一个成员
+    public class Inner08 {//成员内部类
+        private int n1 = 99;
+
+        public void say() {
+            System.out.println("n1 = " + n1 + " name = " + name + " 外部类的n1=" + Outer08.this.n1);
+            hi();
+        }
+    }
+
+    //写方法
+    public void t1() {
+        //使用成员内部类
+        //创建成员内部类的对象，然后使用相关的方法
+        Inner08 inner08 = new Inner08();
+        inner08.say();
+        System.out.println(inner08.n1);
+    }
+}
