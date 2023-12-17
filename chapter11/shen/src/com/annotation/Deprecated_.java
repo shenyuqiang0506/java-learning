f6a7c344f0d635654c35a61efcbaf61fc4e6bdf0
山河不入心 <2861476552@qq.com>
2023-12-17 15:55:44 +0800

    Deprecated

--- chapter11/shen/src/com/annotation/Deprecated_.java
+++ chapter11/shen/src/com/annotation/Deprecated_.java
@@ -5,4 +5,32 @@ package com.annotation;
  * @Date 2023/12/13 11:55
  */
 public class Deprecated_ {
+    public static void main(String[] args) {
+        A a = new A();
+        a.hi();
+        System.out.println(a.n1);
+    }
+}
+//1. @Deprecated 修饰某个元素, 表示该元素已经过时
+//2. 即不在推荐使用，但是仍然可以使用
+//3. 查看 @Deprecated 注解类的源码
+//4. 可以修饰方法，类，字段, 包, 参数  等等
+//5. @Deprecated 可以做版本升级过渡使用
+/*
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD, PACKAGE, PARAMETER, TYPE})
+public @interface Deprecated {
+}
+ */
+
+@Deprecated
+class A {
+    @Deprecated
+    public int n1 = 10;
+
+    @Deprecated
+    public void hi() {
+
+    }
 }
