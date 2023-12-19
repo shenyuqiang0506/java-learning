6ad19b80ff503f2478395843e31c2cc48847cbce
山河不入心 <2861476552@qq.com>
2023-12-19 16:14:42 +0800

    NumberFormatException

---
+++ chapter12/Shen/src/com/exception_/NumberFormatException_.java
@@ -0,0 +1,13 @@
+package com.exception_;
+
+/**
+ * @Author 申宇强
+ * @Date 2023/12/19 16:05
+ */
+public class NumberFormatException_ {
+    public static void main(String[] args) {
+        String name = "申宇强";
+        int num = Integer.parseInt(name);
+        System.out.println(num);
+    }
+}
