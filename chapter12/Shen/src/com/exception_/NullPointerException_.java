5f15c68d0d24e09567f0654c4670612316b2f57f
山河不入心 <2861476552@qq.com>
2023-12-19 16:07:36 +0800

    空指针异常NullPointerException

---
+++ chapter12/Shen/src/com/exception_/NullPointerException_.java
@@ -0,0 +1,12 @@
+package com.exception_;
+
+/**
+ * @Author 申宇强
+ * @Date 2023/12/19 16:05
+ */
+public class NullPointerException_ {
+    public static void main(String[] args) {
+        String name = null;
+        System.out.println(name.length());
+    }
+}
