68ac206dd8a458eaf42918502a215fdfd50b75fd
山河不入心 <2861476552@qq.com>
2023-12-19 16:12:45 +0800

    ClassCastException

---
+++ chapter12/Shen/src/com/exception_/ClassCastException_.java
@@ -0,0 +1,22 @@
+package com.exception_;
+
+/**
+ * @Author 申宇强
+ * @Date 2023/12/19 16:05
+ */
+public class ClassCastException_ {
+    public static void main(String[] args) {
+        A b = new B(); //向上转型
+        B b2 = (B) b;//向下转型，这里是OK
+        C c2 = (C) b;//这里抛出ClassCastException
+    }
+}
+
+class A {
+}
+
+class B extends A {
+}
+
+class C extends A {
+}
