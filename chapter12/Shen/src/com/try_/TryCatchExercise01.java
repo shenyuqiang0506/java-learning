50aa17b98442dca3f05eb94d511abd5b3ef77447
山河不入心 <2861476552@qq.com>
2023-12-25 15:29:43 +0800

    try-catch异常处理练习

---
+++ chapter12/Shen/src/com/try_/TryCatchExercise01.java
@@ -0,0 +1,33 @@
+package com.try_;
+
+/**
+ * @Author 申宇强
+ * @Date 2023/12/25 15:18
+ */
+public class TryCatchExercise01 {
+}
+
+class Exception01 {
+    public static int method() {
+        try {
+            String[] names = new String[3];//String[]数组
+            if (names[1].equals("tom")) {//NullPointerException
+                System.out.println(names[1]);
+            } else {
+                names[3] = "shen";
+            }
+            return 1;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            return 2;
+        } catch (NullPointerException e) {//捕获
+            return 3;
+        } finally { //必须执行
+            return 4; //返回4
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(method()); //4
+    }
+}
+
