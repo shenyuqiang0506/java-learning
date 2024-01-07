a50f9159fd25e8c58d0f62720b661f7ee7100b67
山河不入心 <2861476552@qq.com>
2024-01-07 22:55:44 +0800

    作业

---
+++ chapter12/Shen/src/com/homework/Homework04.java
@@ -0,0 +1,23 @@
+package com.homework;
+
+/**
+ * @Author 申宇强
+ * @Date 2024/1/7 22:49
+ */
+public class Homework04 {
+    public static void main(String[] args) {//main方法
+        try {
+            showExce();
+            System.out.println("A");
+        } catch (Exception e) {
+            System.out.println("B");
+        } finally {
+            System.out.println("C");
+        }
+        System.out.println("D");
+    }
+
+    public static void showExce() throws Exception {
+        throw new Exception();
+    }
+}
