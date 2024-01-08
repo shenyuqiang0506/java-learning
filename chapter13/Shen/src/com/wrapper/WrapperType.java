98a98363ef5bffc62ffdc5cbb8f257d64320b811
山河不入心 <2861476552@qq.com>
2024-01-08 10:39:19 +0800

    八个包装类

---
+++ chapter13/Shen/src/com/wrapper/WrapperType.java
@@ -0,0 +1,18 @@
+package com.wrapper;
+
+/**
+ * @Author 申宇强
+ * @Date 2024/1/8 10:36
+ */
+public class WrapperType {
+    public static void main(String[] args) {
+        //boolean -> Boolean
+        //char -> Character
+        //byte -> Byte
+        //int -> Integer
+        //long -> long
+        //float -> Float
+        //double -> Double
+        //short -> Short
+    }
+}
