baadf224eefb14bb6047270704df8e2d717d7bd1
山河不入心 <2861476552@qq.com>
2024-01-08 10:56:57 +0800

    包装类型和string类型相互转换

--- chapter13/Shen/src/com/wrapper/WrapperVSString.java
+++ chapter13/Shen/src/com/wrapper/WrapperVSString.java
@@ -5,4 +5,19 @@ package com.wrapper;
  * @Date 2024/1/8 10:52
  */
 public class WrapperVSString {
+    public static void main(String[] args) {
+        //包装类(Integer)->String
+        Integer i = 100;
+        //方式1
+        String s1 = i + "";
+        //方式2
+        String s2 = i.toString();
+        //方式3
+        String s3 = String.valueOf(i);
+        //String -> 包装类(Integer)
+        String str4 = "12345";
+        Integer n1 = Integer.parseInt(str4);
+//        Integer i3 = new Integer(str4);//构造器
+        System.out.println("ok~~");
+    }
 }
