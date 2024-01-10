a3e5253d870a679adf2ed379345a415062295d3a
山河不入心 <2861476552@qq.com>
2024-01-10 13:38:24 +0800

    StringBuilder

---
+++ chapter13/Shen/src/com/stringbuilder_/StringVsStringBufferVsStringBuilder.java
@@ -0,0 +1,39 @@
+package com.stringbuilder_;
+
+/**
+ * @Author 申宇强
+ * @Date 2024/1/10 13:25
+ */
+public class StringVsStringBufferVsStringBuilder {
+    public static void main(String[] args) {
+        long startTime = 0L;
+        long endTime = 0L;
+        StringBuffer buffer = new StringBuffer("");
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 800000; i++) {//StringBuffer 拼接 80000次
+            buffer.append(String.valueOf(i));
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("StringBuffer的执行时间：" + (endTime - startTime));
+
+
+        StringBuilder builder = new StringBuilder("");
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 800000; i++) {//StringBuilder 拼接 80000次
+            builder.append(String.valueOf(i));
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("StringBuilder的执行时间：" + (endTime - startTime));
+
+
+        String text = "";
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < 800000; i++) {//String 拼接 80000次
+            text = text + i;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("String的执行时间：" + (endTime - startTime));
+
+    }
+}
