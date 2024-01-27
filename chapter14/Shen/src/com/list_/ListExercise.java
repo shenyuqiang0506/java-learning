541c6d0c6a59f0d8cce2c0e6e44828f8ddfafc88
山河不入心 <2861476552@qq.com>
2024-01-27 20:31:03 +0800

    List方法练习

---
+++ chapter14/Shen/src/com/list_/ListExercise.java
@@ -0,0 +1,42 @@
+package com.list_;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @Author 申宇强
+ * @Date 2024/1/27 20:26
+ */
+@SuppressWarnings({"all"})
+public class ListExercise {
+    public static void main(String[] args) {
+        /*
+        添加10个以上的元素(比如String "hello" )，在2号位插入一个元素"韩顺平教育"，
+        获得第5个元素，删除第6个元素，修改第7个元素，在使用迭代器遍历集合，
+        要求:使用List的实现类ArrayList完成。
+         */
+        List list = new ArrayList();
+        for (int i = 0; i < 12; i++) {
+            list.add("hello" + i);
+        }
+        System.out.println("list=" + list);
+        //在2号位插入一个元素"shen"
+        list.add(1, "shen");
+        System.out.println("list=" + list);
+        //获得第5个元素
+        System.out.println("第五个元素=" + list.get(4));
+        //删除第6个元素
+        list.remove(5);
+        System.out.println("list=" + list);
+        //修改第7个元素
+        list.set(6, "三国演义");
+        System.out.println("list=" + list);
+        //在使用迭代器遍历集合
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object obj =  iterator.next();
+            System.out.println("obj=" + obj);
+        }
+    }
+}
