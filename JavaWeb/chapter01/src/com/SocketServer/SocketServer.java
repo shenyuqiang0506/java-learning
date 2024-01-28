3d757464a3b6f0e5c08010c9219efd79c5c4609d
山河不入心 <2861476552@qq.com>
2024-01-28 17:11:33 +0800

    使用浏览器访问Socket服务器

---
+++ JavaWeb/chapter01/src/com/SocketServer/SocketServer.java
@@ -0,0 +1,28 @@
+package com.SocketServer;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @Author 申宇强
+ * @Date 2024/1/28 17:08
+ */
+public class SocketServer {
+    public static void main(String[] args) {
+        try(ServerSocket server = new ServerSocket(8080)){    //将服务端创建在端口8080上
+            System.out.println("正在等待客户端连接...");
+            Socket socket = server.accept();
+            System.out.println("客户端已连接，IP地址为："+socket.getInetAddress().getHostAddress());
+            InputStream in = socket.getInputStream();  //通过
+            System.out.println("接收到客户端数据：");
+            while (true){
+                int i = in.read();
+                if(i == -1) break;
+                System.out.print((char) i);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
