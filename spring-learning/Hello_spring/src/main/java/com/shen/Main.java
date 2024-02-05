12c7f1b7db2db8830225668785c8de2bc97abe5f
山河不入心 <2861476552@qq.com>
2024-02-05 15:43:39 +0800

    Spring入门

---
+++ spring-learning/Hello_spring/src/main/java/com/shen/Main.java
@@ -0,0 +1,17 @@
+package com.shen;
+
+import com.shen.entity.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author 申宇强
+ * @Date 2024/2/4 13:48
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+        Student student = context.getBean(Student.class);
+        student.hello();
+    }
+}
---
+++ spring-learning/Hello_spring/src/main/java/com/shen/entity/Student.java
@@ -0,0 +1,11 @@
+package com.shen.entity;
+
+/**
+ * @Author 申宇强
+ * @Date 2024/2/4 13:51
+ */
+public class Student {
+    public void hello() {
+        System.out.println("hello world");
+    }
+}
