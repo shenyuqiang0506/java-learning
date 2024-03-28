3ac7051940e59ba791f666dbc7888211e4e64a13
山河不入心 <2861476552@qq.com>
2024-03-28 08:53:51 +0800

    邮箱发送模块

---
+++ spring-learning/frameworks/src/main/java/com/shen/FrameworksApplication.java
@@ -0,0 +1,13 @@
+package com.shen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworksApplication.class, args);
+    }
+
+}
