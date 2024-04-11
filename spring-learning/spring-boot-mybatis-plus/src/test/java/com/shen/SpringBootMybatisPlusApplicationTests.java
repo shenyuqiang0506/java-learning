5f605fa6ccfeff9422aecd485caab329d598b30d
山河不入心 <2861476552@qq.com>
2024-04-11 08:55:48 +0800

    一些别的方法insert

--- spring-learning/spring-boot-mybatis-plus/src/test/java/com/shen/SpringBootMybatisPlusApplicationTests.java
+++ spring-learning/spring-boot-mybatis-plus/src/test/java/com/shen/SpringBootMybatisPlusApplicationTests.java
@@ -1,5 +1,6 @@
 package com.shen;
 
+import com.shen.entity.User;
 import com.shen.mapper.UserMapper;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,18 @@ class SpringBootMybatisPlusApplicationTests {
 
     @Test
     void contextLoads() {
-        System.out.println(mapper.selectById(1));  //同样可以直接selectById，非常快速方便
+       System.out.println(mapper.selectById(1));  //同样可以直接selectById，非常快速方便
+//        User user = new User();
+//        user.setName("shen");
+//        user.setEmail("123567@qq.com");
+//        user.setPassword("123456");
+//        int count = mapper.insert(user);
+//        if (count > 0) {
+//            System.out.println("插入成功");
+//        } else {
+//            System.out.println("插入失败");
+//
+//        }
     }
 
 }
